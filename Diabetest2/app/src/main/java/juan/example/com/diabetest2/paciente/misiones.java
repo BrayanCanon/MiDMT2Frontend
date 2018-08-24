ffcdb8b25006cb40e4e502282078c606637c054c
esteban027 <bermu095@outlook.com>
2018-08-24 17:34:18 -0500

    lista misiones paciente

--- Diabetest2/app/src/main/java/juan/example/com/diabetest2/paciente/misiones.java
+++ Diabetest2/app/src/main/java/juan/example/com/diabetest2/paciente/misiones.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -14,6 +15,14 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 import juan.example.com.diabetest2.PasosMision;
@@ -23,6 +32,8 @@ import juan.example.com.diabetest2.util.Conexion;
 public class misiones extends AppCompatActivity {
     ArrayList<MisionVo> listaMisiones;
     RecyclerView recylerMisiones;
+    AdapterMision adapter;
+    Context vista = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,17 +41,65 @@ public class misiones extends AppCompatActivity {
 
         listaMisiones= new ArrayList<>();
         recylerMisiones=findViewById(R.id.recyclerMisiones);
-       /* llenarMisiones();
+        adapter = new AdapterMision(listaMisiones);
+        recylerMisiones.setAdapter(adapter);
+        recylerMisiones.setItemAnimator(new DefaultItemAnimator());
+        recylerMisiones.setLayoutManager(new LinearLayoutManager(this));
 
-
+        try {
+            llenarMisiones();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
 
-   /* private void llenarMisiones() {
+    private void llenarMisiones() throws IOException {
         ArrayList vacio = new ArrayList<>();
-        new Conexion("")
+        FileInputStream in2 = null;
+        try {
+            in2 = openFileInput("id.dt2");
 
-*/
+
+        ObjectInputStream ois2 = new ObjectInputStream(in2);
+
+            String idGuardado =  ois2.readObject().toString();
+            ArrayList<String> nombres= new ArrayList<>();
+            ArrayList valores= new ArrayList();
+            nombres.add("codPaciente");
+
+            valores.add(idGuardado);
+            new Conexion("consultarMisPaciente", nombres, new Conexion.Comunicado() {
+                @Override
+                public void salidas(String output) {
+                    Gson gson= new Gson();
+
+                    JsonArray arreglo = gson.fromJson(output,JsonArray.class);
+                    JsonObject salida;
+                    JsonObject mision;
+                    JsonObject categoria;
+                    JsonObject nivel;
+                    for(int a=0;a<arreglo.size();a++){
+                        salida = arreglo.get(a).getAsJsonObject();
+                        mision = salida.get("idMision").getAsJsonObject();
+                        categoria = mision.get("idCategoria").getAsJsonObject();
+                        nivel= mision.get("idNivel").getAsJsonObject();
+                        MisionVo mis = new MisionVo(mision.get("nombre").getAsString(),categoria.get("nombreCategoria").getAsString(),nivel.get("idNivel").getAsString());
+                        listaMisiones.add(mis);
+
+
+                    }
+                    adapter.notifyDataSetChanged();
+
+
+                }
+            }).execute(valores);
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
