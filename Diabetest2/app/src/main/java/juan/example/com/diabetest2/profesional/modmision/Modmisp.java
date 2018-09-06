cd7c4005e4e145b1f66a92c96cfd1a1ee04f4941
esteban027 <bermu095@outlook.com>
2018-09-06 11:02:11 -0500

    Parte Maruri

--- Diabetest2/app/src/main/java/juan/example/com/diabetest2/profesional/modmision/Modmisp.java
+++ Diabetest2/app/src/main/java/juan/example/com/diabetest2/profesional/modmision/Modmisp.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import juan.example.com.diabetest2.R;
+import juan.example.com.diabetest2.comunes.Comunidad;
 import juan.example.com.diabetest2.util.Mision;
 
 public class Modmisp extends AppCompatActivity {
@@ -21,12 +22,20 @@ public class Modmisp extends AppCompatActivity {
         startActivity(n);
     }
 
+    public void foros(View v){
+        Intent pasolist=new Intent(this,Comunidad.class);
+        pasolist.putExtra("mision",(Mision)getIntent().getParcelableExtra("mision"));
+        startActivity(pasolist);
+
+    }
+
     public void pasos(View v){
         Intent pasolist=new Intent(this,Pasosmod.class);
         pasolist.putExtra("mision",(Mision)getIntent().getParcelableExtra("mision"));
         startActivity(pasolist);
 
     }
+
 
 
 }
