39e15a5cb1a0d5f2948ec4c8d122071143b4bbec
Unknown <emaruri@unbosque.edu.co>
2018-10-18 12:32:51 -0500

    validaciones y perfil de tratamieento

--- Diabetest2/app/src/main/java/juan/example/com/diabetest2/profesional/Encuesta1.java
+++ Diabetest2/app/src/main/java/juan/example/com/diabetest2/profesional/Encuesta1.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,17 +27,34 @@ public class Encuesta1 extends AppCompatActivity {
     }
 
     public void procesar(View v){
-        ArrayList<String> nombres=new ArrayList<>();
-        ArrayList<String> valores=new ArrayList<>();
-        nombres.add("paciente");valores.add(CrearPaciente.paciente);
-        nombres.add("metas");valores.add(metas.getText().toString());
-        new Conexion("encuesta1", nombres, new Conexion.Comunicado() {
-            @Override
-            public void salidas(String output) {
-                Intent salir=new Intent(este,Encuesta2.class);
-                startActivity(salir);
-            }
-        }).execute(valores);
+        if(!metas.getText().toString().matches("")) {
+            ArrayList<String> nombres = new ArrayList<>();
+            ArrayList<String> valores = new ArrayList<>();
+            nombres.add("paciente");
+            valores.add(CrearPaciente.paciente);
+            nombres.add("metas");
+            valores.add(metas.getText().toString());
+            new Conexion("encuesta1", nombres, new Conexion.Comunicado() {
+                @Override
+                public void salidas(String output) {
+                    Intent salir = new Intent(este, Encuesta2.class);
+                    startActivity(salir);
+                }
+            }).execute(valores);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Por favor complete el diagnóstico!", Toast.LENGTH_SHORT).show();
+        }
 
+    }
+
+    //Advertencia de retroceso
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            Toast.makeText(getApplicationContext(), "Por favor complete el diagnóstico!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
--- Diabetest2/app/src/main/java/juan/example/com/diabetest2/profesional/Encuesta2.java
+++ Diabetest2/app/src/main/java/juan/example/com/diabetest2/profesional/Encuesta2.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,19 +27,36 @@ public class Encuesta2 extends AppCompatActivity {
     }
 
     public void procesar(View v){
-        ArrayList<String> nombres=new ArrayList<>();
-        ArrayList<String> valores=new ArrayList<>();
+        if(!sugerencias.getText().toString().matches("")) {
+            ArrayList<String> nombres = new ArrayList<>();
+            ArrayList<String> valores = new ArrayList<>();
 
-        nombres.add("paciente");valores.add(CrearPaciente.paciente);
-        nombres.add("sugerencias");valores.add(sugerencias.getText().toString());
+            nombres.add("paciente");
+            valores.add(CrearPaciente.paciente);
+            nombres.add("sugerencias");
+            valores.add(sugerencias.getText().toString());
 
-        new Conexion("encuesta2", nombres, new Conexion.Comunicado() {
-            @Override
-            public void salidas(String output) {
-                Intent salir=new Intent(este,MenuProfesional.class);
-                startActivity(salir);
-            }
-        }).execute(valores);
+            new Conexion("encuesta2", nombres, new Conexion.Comunicado() {
+                @Override
+                public void salidas(String output) {
+                    Intent salir = new Intent(este, MenuProfesional.class);
+                    startActivity(salir);
+                }
+            }).execute(valores);
+        }
 
+        else {
+            Toast.makeText(getApplicationContext(), "Por favor complete el diagnóstico!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    //Advertencia de retroceso
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            Toast.makeText(getApplicationContext(), "Por favor complete el diagnóstico!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
